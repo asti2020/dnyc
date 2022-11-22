a89169aa5368d297bd87900a6cf48feabf1f3f6a
Astika Luintel <astild2020@gmail.com>
2022-11-22 16:28:59 -0500

    the initial for detabase

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Creating users..."
+User.create!(first_name: "Example User", email: "exe@exe.com")
+puts "done seeding..."
